f8f7516bdde767a85dd4ae92782e1904bd372877
sasnigdho <salmanahmedims@gmail.com>
2023-06-17 00:08:06 +0600

    Splash page added.

---
+++ lib/app/ui/pages/splash_page/splash_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../controllers/splash_controller.dart';
+
+class SplashPage extends GetView<SplashController> {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SplashPage'),
+      ),
+      body: const SafeArea(
+        child: Text('SplashController'),
+      ),
+    );
+  }
+}
