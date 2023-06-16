3ab11631b2efbaf43f668e11c6ebfeab34dfd222
sasnigdho <salmanahmedims@gmail.com>
2023-06-17 00:39:01 +0600

    Splash page added in pages.

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../bindings/home_binding.dart';
+import '../bindings/splash_binding.dart';
 import '../bindings/steps_binding.dart';
 import '../ui/pages/home_page/home_page.dart';
+import '../ui/pages/splash_page/splash_page.dart';
 import '../ui/pages/steps_page/steps_page.dart';
 import 'route_names.dart';
 
@@ -17,14 +20,21 @@ class AppPages {
       ),
     ),
     GetPage(
+      name: RouteNames.splash,
+      binding: SplashBinding(),
+      page: () => const SplashPage(),
+      transition: _defaultTransition,
+    ),
+    GetPage(
       name: RouteNames.home,
+      binding: HomeBinding(),
       page: () => const HomePage(),
       transition: _defaultTransition,
     ),
     GetPage(
       name: RouteNames.steps,
-      page: StepsPage.new,
       binding: StepsBinding(),
+      page: StepsPage.new,
       transition: _defaultTransition,
     ),
   ];
