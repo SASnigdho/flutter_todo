e50076482545a978b3341ca5b8c87916a033f30b
sasnigdho <salmanahmedims@gmail.com>
2023-06-17 00:12:28 +0600

    GetMaterialApp used in todo.

--- lib/app/todo.dart
+++ lib/app/todo.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
+import 'routes/app_pages.dart';
+import 'routes/route_names.dart';
 import 'ui/pages/home_page/home_page.dart';
 
 class Todo extends StatelessWidget {
@@ -7,8 +10,16 @@ class Todo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'TODO App',
+      debugShowCheckedModeBanner: false,
+
+      // Routing.
+      initialRoute: RouteNames.splash,
+      getPages: AppPages.pages,
+      builder: (_, child) => child!,
+      unknownRoute: AppPages.pages.first,
+
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
