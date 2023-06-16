35f88a630880fdd05a12ada5af272dbb9b3854df
sasnigdho <salmanahmedims@gmail.com>
2023-06-17 00:18:29 +0600

    App Colors added.

---
+++ lib/app/const/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF528A2E);
+  static const Color lightBg = Color(0xFFFFFFFF);
+  static const Color red = Color(0xFFD11212);
+}
